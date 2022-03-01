789b343b24437bf73a60f2d48a0f09b663577018
Grigor Georgiev <contact@grigorgeorgiev.com>
2022-03-01 14:08:57 +0200

    Update using-misqlrecord.php

--- miphpf-manual-and-examples/using-misqlrecord.php
+++ miphpf-manual-and-examples/using-misqlrecord.php
@@ -1,1 +1,34 @@
+<?php
 
+//miSqlRecord is an active record class. It can be used to read, insert, update and delete a specific record from the database. When reading if the record cannot be found in the database, miDBException will be thrown.
+
+//* Reading a record by primary key example:
+try  {
+    $sqlRecord = new miSqlRecord('Customers', 'CustomerID');
+    $sqlRecord->readPK(6);
+    // echo '<pre>';
+    // print_r($sqlRecord);
+    // echo '</pre>';
+    // miSqlRecord Object
+    // (
+    //     [_row:protected] => Array
+    //         (
+    //             [CustomerID] => 6
+    //             [CustomerEmail] => test123@gmail.com
+    //             [CustomerPassword] => 5fcovwnA67QQ4oN/WhwupQ==$f26db146cc00678ed1f990a2383
+    //             [CustomerPasswordHashMode] => 1
+    //             [CustomerStatus] => 1
+    //             [CustomerGroupID] => 1
+    //             [CustomerBonusPoints] => 0
+    //             [CustomerReferrerID] => 
+    //             [CustomerMembershipNumber] => 
+    //         )
+    
+    //     [_table:protected] => Customers
+    //     [_primaryKey:protected] => CustomerID
+    // )
+}
+catch (miDBException $exception)
+{
+    // Error handling here ...
+}
