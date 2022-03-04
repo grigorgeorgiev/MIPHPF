9cd4cb20545279f4dde4ce8d739c8591a5ccd132
Grigor Georgiev <contact@grigorgeorgiev.com>
2022-03-04 11:01:45 +0200

    Create configuration.php

---
+++ miphpf-manual-and-examples/configuration.php
@@ -0,0 +1,12 @@
+<!-- Framework configuration resides in Config.inc file in the framework installation folder. In the configuration file are set the following settings:
+
+MI_DEFAULT_DB_USER - The default username used for database connections
+MI_DEFAULT_DB_PASS - The default password used for database connections
+MI_DEFAULT_DB_HOST - The default database server host
+MI_DEFAULT_DB_NAME - The default database name
+
+MI_RECORDS_PER_PAGE - Default number of records showed by the table pager
+MI_DEFAULT_LANGUAGE - The default language used for internationalization
+MI_TEMPLATE_BASEDIR - The template directory
+
+The configuration constants can be changed at any time through the miSettings class -->
