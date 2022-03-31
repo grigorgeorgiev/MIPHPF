da6e6bd9d2f98d90937889b3d07529e11a1f8f1d
Grigor Georgiev <contact@grigorgeorgiev.com>
2022-03-31 13:50:06 +0300

    Update miSqlComplexPKRecord.php

--- miphpf-manual-and-examples/miSqlComplexPKRecord.php
+++ miphpf-manual-and-examples/miSqlComplexPKRecord.php
@@ -7,3 +7,29 @@
 
 $record = new miSqlComplexPKRecord('Reciepts', array('OrderID', 'InvoiceID'));
 $record->readPK(array('OrderID' => 3, 'InvoiceID' => 4));
+
+//* Inserting new record example: Unlike miSqlRecord, here the insert() method will not return the primary key.
+$record = new miSqlComplexPKRecord('Receipts', array('OrderID', 'InvoiceID'));
+$record->set('OrderID', 3);
+$record->set('InvoiceID', 4);
+$record->set('ReceiptsCreateTime', time());
+$record->insert();
+
+//* Updating a record example:
+$record = new miSqlComplexPKRecord('Receipts', array('OrderID', 'InvoiceID'));
+$record->set('OrderID', 3);
+$record->set('InvoiceID', 4);
+$record->set('ReceiptLastUpdate', time());
+$record->update();
+
+//The following example is also a valid approach:
+$record = new miSqlComplexPKRecord('Receipts', array('OrderID', 'InvoiceID'));
+$record->readPK(array('OrderID' => 3, 'InvoiceID' => 4));
+$record->set('ReceiptsLastUpdate', time());
+$record->update();
+
+//* Deleting a record example:
+$record = new miSqlComplexPKRecord('Receipts', array('OrderID', 'InvoiceID'));
+$record->set('OrderID', 3);
+$record->set('InvoiceID', 4);
+$record->delete();
